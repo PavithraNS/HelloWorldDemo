4c976eea920cb86a792922585c8a8e471fa56dcf
Pavithra <nspavi1991@gmail.com>
2021-10-04 08:38:02 +0530

    [Pavithra Add Addition operation]

--- HelloWorld073/Program.cs
+++ HelloWorld073/Program.cs
@@ -7,14 +7,19 @@ namespace HelloWorld073
     /// </summary>
     class Program
     {
-        /// <summary>
-        /// Defines the entry point of the application.
-        /// </summary>
+        /// <summary>Defines the entry point of the application.</summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Fellowship");
+            Addition();
             Console.ReadLine();
         }
+
+        public static void Addition()
+        {
+            Console.WriteLine("Addition operation");
+        }
+
     }
 }
